75b508ff773f0f72adf21e0d46d4c2240d307b85
Denis-Hish <hish.denis@gmail.com>
2024-12-06 17:01:33 +0100

    Small text color changes

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -9,16 +9,14 @@ const Login = () => {
     >
       <div className="container h-100">
         <div className="d-flex flex-column justify-content-center align-items-center">
-          <h2 className="text-center text-white mb-0">
+          <h2 className="text-center mb-0">
             {t('Welcome to')}
             <br />
             <span className="app-name my-3 d-inline-block text-primary">
               VacatioN CaleNdaR
             </span>
           </h2>
-          <small className="text-center text-white mt-3 mb-2">
-            {t('Sign in with')}
-          </small>
+          <small className="text-center mt-3 mb-2">{t('Sign in with')}</small>
           <GoogleButton />
         </div>
       </div>
